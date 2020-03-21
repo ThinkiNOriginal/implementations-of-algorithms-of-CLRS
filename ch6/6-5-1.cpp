633a4244bc05a33db537db58c6157676081ada05
Lotus D <DingLQ@protonmail.com>
2020-03-21 22:56:03 +0800

    delete MaxHeap function

--- ch6/6-5-1.cpp
+++ ch6/6-5-1.cpp
@@ -6,12 +6,12 @@ using namespace std;
 const int INF = 1 << 31;
 
 int HeapExtractMax(vector<int>& A, int heapSize);
-void BuildMaxHeap(vector<int>& A);
+//void BuildMaxHeap(vector<int>& A);
 
 int main() {
 	vector<int> vec{INF, 15, 13, 9, 5, 12, 8, 7, 4, 0, 6, 2, 1};
 	int heapSize = vec.size() - 1;
-	BuildMaxHeap(vec);
+	//BuildMaxHeap(vec);
 	while (heapSize >= 1) {
 		printf("%d\n", HeapExtractMax(vec, heapSize));
 		heapSize--;
